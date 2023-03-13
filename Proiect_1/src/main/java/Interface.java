531a58d9426ed01fff353b5f4981741bb88f0766
Szili <sziliszoll54@gmail.com>
2023-03-13 15:32:22 +0200

    Am implementat operatiile de adunare si scadere & am inceput interfata grafica

--- Proiect_1/src/main/java/Interface.java
+++ Proiect_1/src/main/java/Interface.java
@@ -1,4 +1,17 @@
-
+import javax.swing.*;
+import java.awt.*;
 
 public class Interface {
+
+      Interface() {
+          JFrame frame = new JFrame("Frame");
+          frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+          frame.setSize(500, 500);
+          frame.setLayout(null);
+          frame.setVisible(true);
+          JButton adun = new JButton("Adunare");
+          JTextField tf1 = new JTextField();
+          adun.setBounds(200,200,70,50);
+          frame.add(adun);
+      }
 }
--- Proiect_1/src/main/java/MainClass.java
+++ Proiect_1/src/main/java/MainClass.java
@@ -1,10 +1,12 @@
 public class MainClass {
     public static void main(String[] Args){
-        Monom mon1=new Monom(2,3);
+       /* Monom mon1=new Monom(2,3);
         Monom mon2=new Monom (3,1);
         Polinom pol = new Polinom(0,mon1);
         pol.add(1,mon2);
         Polinom pol2 = new Polinom(0,mon1);
-        pol2.add(1,mon1);
+        pol2.add(1,mon1);*/
+        Interface anInterface = new Interface();
+
     }
 }
